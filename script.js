f50ae600b3d61f01ca7fbb95df5de7b0aceca1ab
Alex <cernikov.alexandr.80@gmail.com>
2022-11-09 22:22:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const arr = ['a','b','c'];
+
+const arrObj = {
+  0:'a',
+  1:'b',
+  2:'c'
+};
+
+console.log(arr[1])
+console.log(arrObj[0])
+arrObj.new = 'NEW';
+console.log(arrObj['new'])
+arr.push('file');
+console.log(arr)
+
+const obj ={
+  Anna: 500,
+  'Alice': 800
+}
+console.log(obj.Alice)
+
+const storeName = 'Linella';
+const storeDescription = {
+    budget: 10000,
+    employees:['Alex','John','Anna'],
+    products:{
+        milk: 10,
+        hleb:20
+    },
+    open: true
+};
+
+console.log(storeDescription)
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+const user = 'Alex';
+alert(`Hello, ${user}`)
